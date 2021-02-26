61400c8e436e2a7a6411c6307f9b183bbf90c759
JasvinderSingh-Jassi <51856951+JasvinderSingh-Jassi@users.noreply.github.com>
2021-02-26 19:59:41 +0530

    Add files via upload

---
+++ TestHub-master/Test Cases/XYZ_Bank_spec/XYZ Bank_HomePage_spec.js
@@ -0,0 +1,39 @@
+//Accessing the variables from XYZ_Bank_Page.js
+let XYZ_Bank_Page = require('../../Page Objects/XYZ_Bank_Page');
+
+
+describe("XYZ Bank Home Page",() =>
+{
+   beforeEach(() =>{
+
+      //Access the URL
+      browser.get('http://www.way2automation.com/angularjs-protractor/banking/#/login');
+
+      //Maximize the browser window
+      browser.manage().window().maximize();
+   });
+    
+
+   it("click Home Page",() =>{
+
+    //click on the home button
+    XYZ_Bank_Page.home.click();
+
+    //Verifying the home page heading
+    expect(XYZ_Bank_Page.header.getText()).toEqual("XYZ Bank");
+    
+    //click on the customer login
+    XYZ_Bank_Page.Customer_Login.click();
+    
+
+    //click on home page
+     XYZ_Bank_Page.home.click();
+
+    //click on the manager login
+    XYZ_Bank_Page.Manager_Login.click();
+
+    //click on home page
+    XYZ_Bank_Page.home.click();
+})
+
+})
